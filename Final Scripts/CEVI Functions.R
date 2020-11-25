44d20443b7f25695e6b9d3f93557a78f96a31dc9
EPOPEA <66261912+epopea@users.noreply.github.com>
2020-11-25 10:36:05 -0300

    Add files via upload

---
+++ Final Scripts/CEVI Functions.R
@@ -0,0 +1,354 @@
+#----------Functions-------------------
+
+cevi <- function(data=NULL,
+                 formula=NULL,
+                 subgroup=NULL,
+                 cutoffs=NULL,
+                 weight=NULL,
+                 dimw=NULL,
+                 k=NULL,
+                 g=0,
+                 na.rm=TRUE,
+                 index=TRUE,
+                 decompose=TRUE,
+                 decomp.complete=TRUE,
+                 ...){
+  myPackages <- c("convey", "survey")
+  new.packages <- myPackages[!(myPackages %in% installed.packages()[,"Package"])]
+  if(length(new.packages)) install.packages(new.packages)
+  lapply(myPackages, require, character.only = TRUE)
+  
+  output <- list(Index="",
+                 Decomposition="",
+                 Detailed.Decomposition="")
+  
+  temp <- function(x){
+    A <- bind_cols("Index"="CEVI",
+                   "Estimate"=unlist(x)[1],
+                   "SE"=print(x)[1,2])
+                   #"95% CI - LB"=unlist(x)[1]-print(x)[1,2]*1.96,
+                   #"UB"=unlist(x)[1]+print(x)[1,2]*1.96)
+    B <- bind_cols("Index"="H",
+                   "Estimate"=attr(x, "extra")[1,1],
+                   "SE" = attr(x, "extra")[1,2])
+                   #"95% CI - LB"=attr(x, "extra")[1,1] - attr(x, "extra")[1,2]*1.96,
+                   #"UB"=attr(x, "extra")[1,1]+attr(x, "extra")[1,2]*1.96)
+    C <- bind_cols("Index"="A",
+                   "Estimate"=attr(x, "extra")[2,1],
+                   "SE" = attr(x, "extra")[2,2])
+                   #"95% CI - LB"=attr(x, "extra")[2,1] - attr(x, "extra")[2,2]*1.96,
+                   #"UB"=attr(x, "extra")[2,1]+attr(x, "extra")[2,2]*1.96)
+    z <- bind_rows(A,
+                   B,
+                   C)
+    
+    return(z)
+  }
+  
+  temp2 <- function(x,length=NULL){
+    A = bind_cols("Exposure"=round(sum(x$`percentual contribution per dimension`[1:length[1]])*100,2),
+                  "Suceptibility"=round(sum(x$`percentual contribution per dimension`[sum(length[1],1):sum(length[1],length[2])])*100,2),
+                  "Adaptive Capacity"=round(sum(x$`percentual contribution per dimension`[sum(length[1],length[2],1):sum(length[1],length[2],length[3])])*100,2))
+    
+    return(A)
+  }
+  
+  quiet <- function(x) { 
+    sink(tempfile()) 
+    on.exit(sink()) 
+    invisible(force(x)) 
+  } 
+  
+  if(index==TRUE){
+    cevi <- svyafc(formula = formula,
+                   design = data,
+                   cutoffs = as.list(cutoffs),
+                   subgroup = subgroup,
+                   weigth = weigth,
+                   dimw=dimw,
+                   k = k,
+                   g = g,
+                   na.rm = na.rm
+    )
+    indexed <- quiet(temp(cevi))
+    output[[1]] <- indexed
+  }
+  
+  if(decompose==TRUE){
+    cevi_dec <- svyafcdec(formula = formula,
+                          design = data,
+                          cutoffs = as.list(cutoffs),
+                          subgroup = subgroup,
+                          weigth = weigth,
+                          dimw=dimw,
+                          k = k,
+                          g = g,
+                          na.rm = na.rm
+    )
+    decomposed <- quiet(temp2(cevi_dec, ...))
+    output[[2]] <- decomposed
+  }
+  
+  if(decomp.complete==TRUE){
+    output[[3]] <- cevi_dec
+  }
+  return(output)
+}
+
+# Example of use of the cevi function
+#cevi(data= cdata, formula = formula, cutoffs = cutoffs,
+#     subgroup = ~Regiao, k= .2, g= 0, decompose = TRUE, 
+#     index = TRUE, length = c(7, 4, 4))
+
+sensitivity <- function(data=NULL,
+                        grouping.var=NULL,
+                        weights = 1,
+                        ids = 1,
+                        formula = formula,
+                        cutoffs = NULL,
+                        dimw=NULL,
+                        k=NULL,
+                        g=0,
+                        na.rm=TRUE,
+                        sensitivity=TRUE,
+                        sensitivity.plot=TRUE,
+                        dominance=TRUE,
+                        ...){ # passar argumentos do stargazer
+  
+  
+  myPackages <- c("convey", "survey", "stargazer", "dplyr")
+  new.packages <- myPackages[!(myPackages %in% installed.packages()[,"Package"])]
+  if(length(new.packages)) install.packages(new.packages)
+  lapply(myPackages, require, character.only = TRUE)
+  
+  
+  cdata <- list()
+  cevi <- list()
+  sensitivity_list <- list()
+  
+  for(i in 1:nrow(unique(data[grouping.var]))){
+    data[pull(data, grouping.var) == unique(pull(data, grouping.var))[i], ] %>% 
+      svydesign(ids = as.formula(paste0("~", ids)),
+                weights = as.formula(paste0("~", weights)),
+                data = .) %>% 
+      convey_prep() -> cdata[[i]]
+    for(ks in seq(0.05, 1, 0.05)){
+      cevi[[ks*20]] <-
+        svyafc(
+          formula,
+          design = cdata[[i]],
+          cutoffs = as.list(cutoffs),
+          subgroup = as.formula(paste0("~", grouping.var)),
+          dimw = dimw,
+          k = ks, g = g,
+          na.rm = na.rm
+        )
+      if(ks == 0.05){
+        sensitivity_list[[i]] <- data.frame( 
+          "k" = ks , 
+          "CEVI Estimate" = coef( cevi[[ks*20]] ) ,
+          "CEVI Std. Error" = SE( cevi[[ks*20]] ) ,
+          "Headcount Estimate" = attr( cevi[[ks*20]] , "extra" )[ 1 , 1 ] ,
+          "Headcount Std Error" = attr( cevi[[ks*20]] , "extra" )[ 1 , 2 ] ,
+          "Intensity Estimate" = attr( cevi[[ks*20]] , "extra" )[ 2 , 1 ] ,
+          "Intensity Std Error" = attr( cevi[[ks*20]] , "extra" )[ 2 , 2 ]
+        )
+      } else{
+        sensitivity_list[[i]] <- rbind(
+          sensitivity_list[[i]] ,
+          data.frame( 
+            "k" = ks , 
+            "CEVI Estimate" = coef( cevi[[ks*20]] ) ,
+            "CEVI Std. Error" = SE( cevi[[ks*20]] ) ,
+            "Headcount Estimate" = attr( cevi[[ks*20]] , "extra" )[ 1 , 1 ] ,
+            "Headcount Std Error" = attr( cevi[[ks*20]] , "extra" )[ 1 , 2 ] ,
+            "Intensity Estimate" = attr( cevi[[ks*20]] , "extra" )[ 2 , 1 ] ,
+            "Intensity Std Error" = attr( cevi[[ks*20]] , "extra" )[ 2 , 2 ]
+          )
+        )
+      }
+      
+      
+    }
+    
+    sensitivity_list[[i]]$Intensity.Estimate[is.na(sensitivity_list[[i]]$Intensity.Estimate)] <- 0
+    sensitivity_list[[i]]$Intensity.Std.Error[is.na(sensitivity_list[[i]]$Intensity.Std.Error)] <- 0
+    
+  }
+  
+  overall_data <- data %>% 
+    svydesign(ids = as.formula(paste0("~", ids)),
+              weights = as.formula(paste0("~", weights)),
+              data = .) %>% 
+    convey_prep()
+  sensitivity_overall <- NULL
+  for(ks in seq(0.05, 1, 0.05)){
+    cevi_overall <-
+      svyafc(
+        formula,
+        design = overall_data,
+        cutoffs = as.list(cutoffs),
+        subgroup = as.formula(paste0("~factor(", grouping.var, ")")),
+        dimw = dimw,
+        k = ks, g = g,
+        na.rm = na.rm
+      )
+    
+    sensitivity_overall <- rbind(
+      sensitivity_overall ,
+      data.frame( 
+        "k" = ks , 
+        "CEVI Estimate" = coef( cevi_overall ) ,
+        "CEVI Std. Error" = SE( cevi_overall ) ,
+        "Headcount Estimate" = attr( cevi_overall , "extra" )[ 1 , 1 ] ,
+        "Headcount Std Error" = attr( cevi_overall , "extra" )[ 1 , 2 ] ,
+        "Intensity Estimate" = attr( cevi_overall , "extra" )[ 2 , 1 ] ,
+        "Intensity Std Error" = attr( cevi_overall , "extra" )[ 2 , 2 ]
+      )
+    )
+  }
+  sensitivity_overall$Intensity.Estimate[is.na(sensitivity_overall$Intensity.Estimate)] <- 0
+  sensitivity_overall$Intensity.Std.Error[is.na(sensitivity_overall$Intensity.Std.Error)] <- 0
+  
+  estimate = NULL
+  
+  for(i in 1:length(sensitivity_list)){
+    estimate = c(estimate, sensitivity_list[[i]]$CEVI.Estimate)
+    max_bound <- seq_along(sensitivity_list[[i]]$CEVI.Estimate)[which.min(sensitivity_list[[i]]$CEVI.Estimate) - 1]
+  }
+  
+  vars = as.data.frame(matrix(data = NA, nrow = (max_bound-k), ncol = length(sensitivity_list)))
+  for(i in 1:length(sensitivity_list)){
+    assign(paste0("vars_", i), NULL)
+    for(j in 1:(max_bound - k)){
+      assign(paste0("vars_", i), c(get(paste0("vars_", i)), var(sensitivity_list[[i]]$CEVI.Estimate[j:(j+k)])))
+    }
+    vars[, i] <- get(paste0("vars_", i))
+  }
+  #x <- as.data.frame(matrix(data = NA, nrow = (max_bound-4), ncol = length(sensitivity_list)))
+  
+  #for(i in 1:length(sensitivity_list)){
+  #  for(j in 1:(max_bound-4)){
+  #    x[j, i] <- sensitivity_list[[i]]$CEVI.Estimate[j]
+  #  }
+  #}
+  #diffs <- as.data.frame(matrix(data = NA, nrow = (max_bound-4), ncol = (length(sensitivity_list) -1)))
+  #for(i in 1:(max_bound-4)){
+  #  x[i, ] <- sort(x[i, ], decreasing = TRUE)
+  #  for(j in 1:(length(sensitivity_list) - 1)){
+  #    diffs[i, j] <- x[i, j] - x[i, j+1]
+  #  }
+  #}
+  
+  #means0 = rowSums(diffs)/ncol(diffs)
+  means1 = rowSums(vars)/ncol(vars)
+  #means = vector(length = max_bound-4)
+  #for(i in 1:length(means)){
+  #  means[i] = mean(means0[i], means1[i])
+  #}
+  #best_k_reg = seq(0.05, 1, 0.05)[which.min(means)]
+  best_k_reg = seq(0.05, 1, 0.05)[which.min(means1)]
+  min = min(estimate, na.rm = T)
+  max = max(estimate, na.rm = T)
+  
+  vars_overall = as.data.frame(matrix(data = NA,
+                                      nrow = seq_along(sensitivity_overall$CEVI.Estimate)[which.min(sensitivity_overall$CEVI.Estimate) - 1] - k,
+                                      ncol = 3))
+  
+  for(j in 1:nrow(vars_overall)){
+    vars_overall[j, 1] <- var(sensitivity_overall$CEVI.Estimate[j:(j+k)])
+    vars_overall[j, 2] <- var(sensitivity_overall$Headcount.Estimate[j:(j+k)])
+    vars_overall[j, 3] <- var(sensitivity_overall$Intensity.Estimate[j:(j+k)])
+  }
+  
+  
+  #diffs_overall <- as.data.frame(matrix(data = NA,
+  #                                      nrow = seq_along(sensitivity_overall$CEVI.Estimate)[which.min(sensitivity_overall$CEVI.Estimate) - 1] - 4,
+  #                                      ncol = 2))
+  #for(j in 1:nrow(vars_overall)){
+  #  x <- sort(c(sensitivity_overall$CEVI.Estimate[j], 
+  #              sensitivity_overall$Headcount.Estimate[j],
+  #              sensitivity_overall$Intensity.Estimate[j]))
+  #  diffs_overall[j, ] <- c(x[3]-x[2], x[2] - x[1])
+  #}
+  #means_diffs_overall = rowSums(diffs_overall)/2
+  means_vars_overall = rowSums(vars_overall)/ncol(vars)
+  #means_overall = vector(length = nrow(vars_overall))
+  #for(i in 1:nrow(vars_overall)){
+  #  means_overall[i] <- mean(means_diffs_overall[i], means_vars_overall[i])
+  #}
+  #best_k_ovr = seq(0.05, 1, 0.05)[which.min(means_overall)]
+  best_k_ovr = seq(0.05, 1, 0.05)[which.min(means_vars_overall)]
+  
+  if(dominance == TRUE){
+    tiff(paste("dominance.tiff", sep = ""), width =6, height = 4,
+         units="in", res=300)
+    plot(x=sensitivity_list[[1]]$k,
+         y=sensitivity_list[[1]]$CEVI.Estimate, type="l",col=1,lwd=2,
+         main="Sensitivity Analysis of the Deprivation Cuttoff\nfor CEVI across Macroregions",
+         ylab="Index Estimate",
+         ylim=c(min, max),
+         xlab="Cuttoff (k)",
+         xaxt="n")
+    for(i in 2:length(sensitivity_list)){
+      lines(x=sensitivity_list[[i]]$k,y=sensitivity_list[[i]]$CEVI.Estimate, type="l", col=i,lwd=2)
+    }
+    axis(1, at=seq(0,1,0.05),las=2)
+    legend("topright",legend= unique(pull(data, grouping.var)),
+           col=c(1:length(sensitivity_list)),lwd=rep(2,length(sensitivity_list)))
+    abline(v=c(best_k_reg, best_k_reg+0.05),lty=c(1,1), lwd=1.5)
+    dev.off()
+    cat("\n A file named dominance.tiff has been saved in your working directory")
+  }
+  
+  if(sensitivity.plot == TRUE){
+    tiff("sensitivity.tiff", width = 6, height = 4,
+         units="in", res=300)
+    plot(x=sensitivity_overall$k,
+         y=sensitivity_overall$CEVI.Estimate, type="l",col="blue",lwd=2,
+         #main="Sensitivity Analysis of the Deprivation Cuttoff",
+         ylab="Index Value",
+         xaxt="n",
+         ylim=c(min(sensitivity_overall$Intensity.Estimate,
+                    sensitivity_overall$Headcount.Estimate,
+                    sensitivity_overall$CEVI.Estimate, na.rm = TRUE),
+                max(sensitivity_overall$Intensity.Estimate,
+                    sensitivity_overall$Headcount.Estimate,
+                    sensitivity_overall$CEVI.Estimate, na.rm = TRUE)),
+         xlab="Cuttoff (k)")
+    axis(1, at=seq(0,1,0.05),las=2)
+    lines(x=sensitivity_overall$k,y=sensitivity_overall$Headcount.Estimate, type="l", col="red",lwd=2)
+    lines(x=sensitivity_overall$k,y=sensitivity_overall$Intensity.Estimate, type="l",col="green",lwd=2)
+    legend("topright",legend=c("CEVI","Censored Headcount","Vulnerability Intensity"),
+           col=c("blue","red","green"),lwd=rep(2,3), cex=0.7,
+           bty='n')
+    abline(v=c(best_k_ovr,best_k_ovr+0.05),lty=c(1,2))
+    #arrows(x0 = 0.35,y0 = 0.2,x1 = 0.275, y1=0.09, lwd=2)
+    #text(x= 0.5, y= 0.21, "Stabilizing region")
+    dev.off()
+    cat("\n A file named sensitivity.tiff has been saved in your working directory")
+  }
+  
+  if(sensitivity == TRUE){
+    cat("\n Vulnerability estimates by varying cutoff levels - Overall study area, Brazil, 2010")
+    stargazer::stargazer(sensitivity_overall, ...)
+    for(i in 1:length(sensitivity_list)){
+      cat(paste0("\n Vulnerability estimates by varying cutoff levels - ",
+                 unique(pull(select(data, grouping.var)))[i],
+                 ", Brazil, 2010"))
+      stargazer::stargazer(sensitivity_list[[i]], ... )
+    }
+  }
+  
+}
+
+# Example of use of the sensitivity function
+#sensitivity(data = data, grouping.var = as.name("Regiao"), weights = 1,ids = 1, 
+#            formula = ~ TXxd + TNxd + TX90pd + TN90pd + DTRd + Cddd + R99pd +
+#              elderly + children + pcincome + literate +
+#              sewage + water + garbage + urb,
+#            cutoffs = c(expcut, suscut, adapcut),
+#            dimw = c(rep(0.04761905,7),rep(0.08333333,8)),
+#            g = 0,
+#            na.rm = T, sensitivity = FALSE, sensitivity.plot = TRUE, dominance = TRUE, summary = FALSE, type = "text")
+
---
+++ Final Scripts/Final Script.R
@@ -0,0 +1,338 @@
+# CEVI - Climate Extreme Vulnerability Index #
+
+# Date: Nov 10, 2020
+# Programmers' email: gilvan@epopea.com.br
+
+#------Setup--------
+# Initial settings #
+#------------------#
+
+# Cleaning the environment
+rm(list=ls())
+
+# Installing the required packages
+myPackages <- c("convey", "dplyr", "magrittr", "survey", "ggplot2", "fmsb", "gtools")
+new.packages <- myPackages[!(myPackages %in% installed.packages()[,"Package"])]
+if(length(new.packages)) install.packages(new.packages)
+lapply(myPackages, require, character.only = TRUE)
+
+# Defining the path and reading the data frame
+
+setwd("/Users/grguedes/Dropbox/My Mac (Air-de-Gilvan)/Downloads/CEVI/")
+data <- readxl::read_excel(path="Datasets/dados.xlsx")
+
+# Renaming variable names
+
+names(data)
+
+data <- rename(old = `%_P_Idosa`,
+               kid = `%P.<5 anos`,
+               data, urb = `%P_UB`,
+               water = `Ab.H20(%)`,
+               sewage = `Esg.sanitario(%)`,
+               garbage = `Coleta de lixo(%)`,
+               literate = `P.20-alfab(%)`,
+               pcincome = `Renda_média_per_capita`)
+
+names(data)
+
+#-----Functions--------
+# Functions developed #
+#---------------------#
+
+source("Final Scripts/CEVI Functions.R")
+
+#--------------------Cutoffs---------------------
+# Establishing the uncensored headcount cutoffs #
+#-----------------------------------------------#
+
+source("Final Scripts/Cutoffs.R", encoding = "utf8")
+
+#-------Subetting by region----------
+# Subsetting the datasets by region #
+#-----------------------------------#
+
+data %<>%
+  select(Mesorregião, TXx, TNx, TX90p, TN90p, DTR, Cdd, R99p) %>%
+  mutate_if(.predicate = is.numeric, .funs = function(x){x <= quantile(x, probs = .75)}) %>%
+  mutate_if(.predicate = is.logical, .funs = as.numeric) %>%
+  inner_join(data,suffix = c("d", ""), by = "Mesorregião")
+
+data %<>%
+  filter(Regiao == 1) %>%
+  select(Mesorregião, TXx, TNx, TX90p, TN90p, DTR, Cdd, R99p) %>%
+  mutate_if(.predicate = is.numeric, .funs = function(x){x <= quantile(x, probs = .75)}) %>%
+  mutate_if(.predicate = is.logical, .funs = as.numeric) %>%
+  right_join(data,suffix = c("dno", ""), by = "Mesorregião")
+
+data %<>%
+  filter(Regiao == 2) %>%
+  select(Mesorregião, TXx, TNx, TX90p, TN90p, DTR, Cdd, R99p) %>%
+  mutate_if(.predicate = is.numeric, .funs = function(x){x <= quantile(x, probs = .75)}) %>%
+  mutate_if(.predicate = is.logical, .funs = as.numeric) %>%
+  right_join(data,suffix = c("dne", ""), by = "Mesorregião")
+
+north_data <- data %>% filter(Regiao==1)
+northeast_data <- data %>% filter(Regiao==2)
+
+#------------------Subsetting by disease-----------------------
+# Subsetting the datasets by incidence of infectious diseases #
+#-------------------------------------------------------------#
+
+low_data <- data %>% filter(TIP_Morb <= quantile(TIP_Morb, probs=c(0.20)))
+high_data <- data %>% filter(TIP_Morb > quantile(TIP_Morb, probs=c(0.60)))
+
+#data %>% select(Mesorregião,Regiao,TIP_Morb) %>% filter(TIP_Morb <= quantile(TIP_Morb, probs=c(0.25))) %>% View()
+#data %>% select(Mesorregião,Regiao,TIP_Morb) %>% filter(TIP_Morb > quantile(TIP_Morb, probs=c(0.75))) %>% View()
+
+low_north_data <- north_data %>% filter(TIP_Morb <= quantile(TIP_Morb, probs=c(0.20)))
+high_north_data <- north_data %>% filter(TIP_Morb > quantile(TIP_Morb, probs=c(0.60)))
+
+low_northeast_data <- northeast_data %>% filter(TIP_Morb <= quantile(TIP_Morb, probs=c(0.20)))
+high_northeast_data <- northeast_data %>% filter(TIP_Morb > quantile(TIP_Morb, probs=c(0.60)))
+
+#--------------Svydesign--------------------
+# Declaring the data as a svydesign object #
+#------------------------------------------#
+
+cdata <- svydesign( ids = ~1 , weights=~Total, data = data )
+cnorth_data <- svydesign( ids = ~1 , weights=~Total, data = north_data )
+cnortheast_data <- svydesign( ids = ~1 , weights=~Total, data = northeast_data )
+
+low_cdata <- svydesign( ids = ~1 , weights=~Total, data = low_data )
+low_cnorth_data <- svydesign( ids = ~1 , weights=~Total, data = low_north_data )
+low_cnortheast_data <- svydesign( ids = ~1 , weights=~Total, data = low_northeast_data )
+
+high_cdata <- svydesign( ids = ~1 , weights=~Total, data = high_data )
+high_cnorth_data <- svydesign( ids = ~1 , weights=~Total, data = high_north_data )
+high_cnortheast_data <- svydesign( ids = ~1 , weights=~Total, data = high_northeast_data )
+
+#------------------------------------Convey------------------------------------
+# Converting the data into data appropriate for the use in the convey package #
+#-----------------------------------------------------------------------------#
+
+cdata <- convey_prep(cdata)
+cnorth_data <- convey_prep(cnorth_data)
+cnortheast_data <- convey_prep(cnortheast_data)
+
+low_cdata <- convey_prep(low_cdata)
+low_cnorth_data <- convey_prep(low_cnorth_data)
+low_cnortheast_data <- convey_prep(low_cnortheast_data)
+
+high_cdata <- convey_prep(high_cdata)
+high_cnorth_data <- convey_prep(high_cnorth_data)
+high_cnortheast_data <- convey_prep(high_cnortheast_data)
+
+
+#----Estimation----
+# CEVI ESTIMATION #
+#-----------------#
+
+cevi(data = high_cnortheast_data,
+     formula = ~ TXxd + TNxd + TX90pd + TN90pd + DTRd + Cddd + R99pd +
+       elderly + children + pcincome + literate +
+       sewage + water + garbage + urb,
+     cutoffs = list(expcut[1],expcut[2],expcut[3],expcut[4],expcut[5],expcut[6],expcut[7],
+                    suscut[1],suscut[2],suscut[3],suscut[4],
+                    adapcut[1],adapcut[2],adapcut[3],adapcut[4]),
+     dimw = c(rep(0.04761905,7),rep(0.08333333,8)),
+     subgroup = ~factor(Regiao),
+     k= .25, g= 0, decomp.complete=TRUE, 
+     decompose = TRUE, index = TRUE,
+     length = c(7, 4, 4))
+
+#-----------Sensitivity---------------
+# SENSITIVITY AND DOMINANCE ANALYSIS #
+#------------------------------------#
+
+table(data$Regiao)
+data$Regiao <- factor(data$Regiao,
+                      levels=c(1,2),
+                      labels=c("Amazonia","Northeast"))
+sensitivity(data = data, grouping.var = as.name("Regiao"), weights = ~Total,ids = 1, 
+            formula = ~ TXxd + TNxd + TX90pd + TN90pd + DTRd + Cddd + R99pd +
+            elderly + children + pcincome + literate +
+            sewage + water + garbage + urb,
+            cutoffs = c(expcut, suscut, adapcut),
+            dimw = c(rep(0.04761905,7),rep(0.08333333,8)),
+            g = 0, k=3,
+            na.rm = T, sensitivity = TRUE, sensitivity.plot = TRUE,
+            dominance = TRUE, summary = F, type = "text")
+
+
+#--------------Radar charts--------------
+# GRAPHICAL REPRESENTATION (RADARCHART) #
+#---------------------------------------#
+
+source("Final Scripts/Radar Charts.R")
+
+#---------Decomposition Graphs---------
+# GRAPHICAL REPRESENTATION (BARPLOTS) #
+#-------------------------------------#
+
+source("Final Scripts/Barplot with Multi Legend.R")
+
+#------------------------#
+# DESCRIPTIVE STATISTICS #
+#------------------------#
+
+# Difference-in-means and difference-in-proportions tests
+
+# Exposure
+t.test(x=data$TXx[data$Regiao=="Amazonia"],
+       y=data$TXx[data$Regiao=="Northeast"],
+       alternative = "two.sided",
+       conf.level=0.95,
+       mu=0,
+       var.equal=F)
+
+t.test(x=data$TNx[data$Regiao=="Amazonia"],
+       y=data$TNx[data$Regiao=="Northeast"],
+       alternative = "two.sided",
+       conf.level=0.95,
+       mu=0,
+       var.equal=F)
+
+t.test(x=data$TX90p[data$Regiao=="Amazonia"],
+       y=data$TX90p[data$Regiao=="Northeast"],
+       alternative = "two.sided",
+       conf.level=0.95,
+       mu=0,
+       var.equal=F)
+
+t.test(x=data$TN90p[data$Regiao=="Amazonia"],
+       y=data$TN90p[data$Regiao=="Northeast"],
+       alternative = "two.sided",
+       conf.level=0.95,
+       mu=0,
+       var.equal=F)
+
+t.test(x=data$DTR[data$Regiao=="Amazonia"],
+       y=data$DTR[data$Regiao=="Northeast"],
+       alternative = "two.sided",
+       conf.level=0.95,
+       mu=0,
+       var.equal=F)
+
+t.test(x=data$Cdd[data$Regiao=="Amazonia"],
+       y=data$Cdd[data$Regiao=="Northeast"],
+       alternative = "two.sided",
+       conf.level=0.95,
+       mu=0,
+       var.equal=F)
+
+t.test(x=data$R99p[data$Regiao=="Amazonia"],
+       y=data$R99p[data$Regiao=="Northeast"],
+       alternative = "two.sided",
+       conf.level=0.95,
+       mu=0,
+       var.equal=F)
+
+# Susceptibility
+t.test(x=data$old[data$Regiao=="Amazonia"],
+       y=data$old[data$Regiao=="Northeast"],
+       alternative = "two.sided",
+       conf.level=0.95,
+       mu=0,
+       var.equal=F)
+
+t.test(x=data$kid[data$Regiao=="Amazonia"],
+       y=data$kid[data$Regiao=="Northeast"],
+       alternative = "two.sided",
+       conf.level=0.95,
+       mu=0,
+       var.equal=F)
+
+t.test(x=data$pcincome[data$Regiao=="Amazonia"],
+       y=data$pcincome[data$Regiao=="Northeast"],
+       alternative = "two.sided",
+       conf.level=0.95,
+       mu=0,
+       var.equal=F)
+
+t.test(x=data$literate[data$Regiao=="Amazonia"],
+       y=data$literate[data$Regiao=="Northeast"],
+       alternative = "two.sided",
+       conf.level=0.95,
+       mu=0,
+       var.equal=F)
+
+# Adaptive Capacity
+t.test(x=data$water[data$Regiao=="Amazonia"],
+       y=data$water[data$Regiao=="Northeast"],
+       alternative = "two.sided",
+       conf.level=0.95,
+       mu=0,
+       var.equal=F)
+
+t.test(x=data$sewage[data$Regiao=="Amazonia"],
+       y=data$sewage[data$Regiao=="Northeast"],
+       alternative = "two.sided",
+       conf.level=0.95,
+       mu=0,
+       var.equal=F)
+
+t.test(x=data$garbage[data$Regiao=="Amazonia"],
+       y=data$garbage[data$Regiao=="Northeast"],
+       alternative = "two.sided",
+       conf.level=0.95,
+       mu=0,
+       var.equal=F)
+
+t.test(x=data$urb[data$Regiao=="Amazonia"],
+       y=data$urb[data$Regiao=="Northeast"],
+       alternative = "two.sided",
+       conf.level=0.95,
+       mu=0,
+       var.equal=F)
+
+# Health
+t.test(x=data$TIP_Morb[data$Regiao=="Amazonia"],
+       y=data$TIP_Morb[data$Regiao=="Northeast"],
+       alternative = "two.sided",
+       conf.level=0.95,
+       mu=0,
+       var.equal=F)
+
+# Cutoff values for variables used
+
+quantile(data$TIP_Morb,probs=c(0.2,0.6))
+
+data %>% select(TXx, TNx, TX90p, TN90p, DTR, Cdd, R99p,
+                old, kid) %>%
+  mutate_all(.funs= function(x) {quantile(x, probs=c(0.75))}) %>% head(1) %>%
+  rbind %>% t %>% round(1)
+
+data %>% select(literate,sewage, water, garbage, urb) %>%
+  mutate_all(.funs= function(x) {quantile(x, probs=c(0.25))}) %>% head(1) %>%
+  rbind %>% t %>% round(1)
+
+data %>% filter(Regiao=="Amazonia") %>%
+  select(old, kid) %>%
+  mutate_all(.funs= function(x) {quantile(x,probs=c(0.25))}) %>% summarise_all(.funs=mean)
+
+data %>% filter(Regiao=="Northeast") %>%
+  select(old, kid) %>%
+  mutate_all(.funs= function(x) {quantile(x,probs=c(0.25))}) %>% summarise_all(.funs=mean)
+
+
+data %>% filter(Regiao=="Amazonia") %>%
+  select(TXx, TNx, TX90p, TN90p, DTR, Cdd, R99p,
+         old, kid, pcincome, literate, sewage, water, garbage, urb,
+         TIP_Morb) %>%  as.data.frame() %>% stargazer(type="text",
+                                                      summary=T, omit.summary.stat=c("n",
+                                                                                     "sd", "p25","p75"),
+                                                      digits=1,
+                                                      notes="Source: Xavier et al. (2015); Brazilian Census (IBGE, 2010); SIH/MS (Brazil, 2009/2011)",
+                                                      title="Descriptive Statistics for Indicators in Amazonia, Brazil - 2010")
+
+data %>% filter(Regiao=="Northeast") %>%
+  select(TXx, TNx, TX90p, TN90p, DTR, Cdd, R99p,
+         old, kid, pcincome, literate, sewage, water, garbage, urb,
+         TIP_Morb) %>%  as.data.frame() %>% stargazer(type="text",
+                                                      summary=T, omit.summary.stat=c("n",
+                                                                                     "sd", "p25","p75"),
+                                                      digits=1,
+                                                      notes="Source: Xavier et al. (2015); Brazilian Census (IBGE, 2010); SIH/MS (Brazil, 2009/2011)",
+                                                      title="Descriptive Statistics for Indicators in the Northeast Region, Brazil - 2010")
+
